3ca837c364102298f28b8236c46f369553e82450
Zee Ahmed <zeieshanahmed@icloud.com>
2016-06-07 00:08:16 +0100

    Removed other header for localhost

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.setHeader('Access-Control-Allow-Origin', 'https://react-movie-hub.herokuapp.com');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
